0a4eea3906cbd21f75f25dfdc9f4ac2eb027a0a1
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-08-17 23:32:31 +0300

    First

---
+++ dom-manipulation/script.js
@@ -0,0 +1,38 @@
+const button = document.getElementById("newQuote");
+const quoteGenerator = document.querySelector("h1");
+const quotes = [
+  {
+    text: "The best way to get started is to quit talking and begin doing.",
+    category: "Motivation",
+  },
+  {
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    category: "Inspiration",
+  },
+  {
+    text: "Life is what happens when you’re busy making other plans.",
+    category: "Life",
+  },
+  {
+    text: "The only limit to our realization of tomorrow is our doubts of today.",
+    category: "Motivation",
+  },
+  {
+    text: "In the middle of every difficulty lies opportunity.",
+    category: "Wisdom",
+  },
+];
+function showRandomQuote(category) {
+  if (category != "") {
+    quotes.forEach((quote) => {
+      if (quote.category === category) {
+        quoteGenerator.textContent = quote.text;
+        console.log(quote.text);
+      }
+    });
+  } else {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteGenerator.textContent = quotes[randomIndex].text;
+  }
+}
+button.addEventListener("click", () => showRandomQuote(""));
